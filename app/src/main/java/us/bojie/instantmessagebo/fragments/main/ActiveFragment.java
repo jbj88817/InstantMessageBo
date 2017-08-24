b05ac0230e4176f3ef031fc68508e37055acec99
Bojie Jiang <jiangbojie@gmail.com>
2017-08-24 00:55:34 -0700

    Setup GalleryView to ActiveFragment

--- app/src/main/java/us/bojie/instantmessagebo/fragments/main/ActiveFragment.java
+++ app/src/main/java/us/bojie/instantmessagebo/fragments/main/ActiveFragment.java
@@ -1,12 +1,17 @@
 package us.bojie.instantmessagebo.fragments.main;
 
 
+import butterknife.BindView;
 import us.bojie.common.app.Fragment;
+import us.bojie.common.widget.GalleryView;
 import us.bojie.instantmessagebo.R;
 
 
 public class ActiveFragment extends Fragment {
 
+
+    @BindView(R.id.galleryView)
+    GalleryView mGallery;
 
     public ActiveFragment() {
         // Required empty public constructor
@@ -17,4 +22,14 @@ public class ActiveFragment extends Fragment {
         return R.layout.fragment_active;
     }
 
+    @Override
+    protected void initData() {
+        super.initData();
+        mGallery.setup(getLoaderManager(), new GalleryView.SelectedChangeListener() {
+            @Override
+            public void onSelectedCountChanged(int count) {
+
+            }
+        });
+    }
 }
