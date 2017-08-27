d4672a01a1f1e2883e24258caa023b1c671cc295
Bojie Jiang <jiangbojie@gmail.com>
2017-08-27 12:13:05 -0700

    GalleryFragment logic

--- app/src/main/java/us/bojie/instantmessagebo/fragments/account/UpdateInfoFragment.java
+++ app/src/main/java/us/bojie/instantmessagebo/fragments/account/UpdateInfoFragment.java
@@ -6,6 +6,7 @@ import butterknife.OnClick;
 import us.bojie.common.app.Fragment;
 import us.bojie.common.widget.PortraitView;
 import us.bojie.instantmessagebo.R;
+import us.bojie.instantmessagebo.fragments.media.GalleryFragment;
 
 /**
  * 用户更新信息的界面
@@ -22,5 +23,14 @@ public class UpdateInfoFragment extends Fragment {
 
     @OnClick(R.id.iv_portrait)
     public void onPortraitClicked() {
+        new GalleryFragment()
+                .setListener(new GalleryFragment.OnSelectedListener() {
+                    @Override
+                    public void onSelectedImage(String path) {
+
+                    }
+                    // show 的时候建议使用getChildFragmentManager，
+                    // tag GalleryFragment class 名
+                }).show(getChildFragmentManager(), GalleryFragment.class.getName());
     }
 }
