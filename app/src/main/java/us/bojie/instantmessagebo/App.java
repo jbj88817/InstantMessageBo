3d119c7b310ce9cb465a6f4162e525788ad1844f
Bojie Jiang <jiangbojie@gmail.com>
2017-09-19 23:21:52 -0700

    Start UCrop

---
+++ app/src/main/java/us/bojie/instantmessagebo/App.java
@@ -0,0 +1,10 @@
+package us.bojie.instantmessagebo;
+
+import us.bojie.common.app.MyApplication;
+
+/**
+ * Created by bojiejiang on 9/19/17.
+ */
+
+public class App extends MyApplication {
+}
--- app/src/main/java/us/bojie/instantmessagebo/fragments/account/UpdateInfoFragment.java
+++ app/src/main/java/us/bojie/instantmessagebo/fragments/account/UpdateInfoFragment.java
@@ -1,9 +1,17 @@
 package us.bojie.instantmessagebo.fragments.account;
 
 
+import android.graphics.Bitmap;
+import android.net.Uri;
+
+import com.yalantis.ucrop.UCrop;
+
+import java.io.File;
+
 import butterknife.BindView;
 import butterknife.OnClick;
 import us.bojie.common.app.Fragment;
+import us.bojie.common.app.MyApplication;
 import us.bojie.common.widget.PortraitView;
 import us.bojie.instantmessagebo.R;
 import us.bojie.instantmessagebo.fragments.media.GalleryFragment;
@@ -27,7 +35,20 @@ public class UpdateInfoFragment extends Fragment {
                 .setListener(new GalleryFragment.OnSelectedListener() {
                     @Override
                     public void onSelectedImage(String path) {
+                        UCrop.Options options = new UCrop.Options();
+                        // 设置图片处理的格式JPEG
+                        options.setCompressionFormat(Bitmap.CompressFormat.JPEG);
+                        // 设置压缩后的图片精度
+                        options.setCompressionQuality(96);
+                        // 得到头像的缓存地址
+                        File dPath = MyApplication.getPortraitTmpFile();
 
+                        // 发起剪切
+                        UCrop.of(Uri.fromFile(new File(path)), Uri.fromFile(dPath))
+                                .withAspectRatio(1, 1)
+                                .withMaxResultSize(520, 520)
+                                .withOptions(options)
+                                .start(getActivity());
                     }
                     // show 的时候建议使用getChildFragmentManager，
                     // tag GalleryFragment class 名
