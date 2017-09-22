a5c83d9e46c09f25f345427f3e369dcdb915d99b
Bojie Jiang <jiangbojie@gmail.com>
2017-09-21 21:33:08 -0700

    getAudioTmpFile and showToast in Application

--- common/src/main/java/us/bojie/common/app/MyApplication.java
+++ common/src/main/java/us/bojie/common/app/MyApplication.java
@@ -2,6 +2,11 @@ package us.bojie.common.app;
 
 import android.app.Application;
 import android.os.SystemClock;
+import android.support.annotation.StringRes;
+import android.widget.Toast;
+
+import net.qiujuer.genius.kit.handler.Run;
+import net.qiujuer.genius.kit.handler.runable.Action;
 
 import java.io.File;
 
@@ -20,6 +25,15 @@ public class MyApplication extends Application {
     }
 
     /**
+     * 外部获取单例
+     *
+     * @return Application
+     */
+    public static Application getInstance() {
+        return instance;
+    }
+
+    /**
      * 获取缓存文件夹地址
      *
      * @return 当前APP的缓存文件夹地址
@@ -28,6 +42,11 @@ public class MyApplication extends Application {
         return instance.getCacheDir();
     }
 
+    /**
+     * 获取头像的临时存储文件地址
+     *
+     * @return 临时文件
+     */
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public static File getPortraitTmpFile() {
         // 得到头像目录的缓存地址
@@ -47,4 +66,54 @@ public class MyApplication extends Application {
         File path = new File(dir, SystemClock.uptimeMillis() + ".jpg");
         return path.getAbsoluteFile();
     }
+
+    /**
+     * 获取声音文件的本地地址
+     *
+     * @param isTmp 是否是缓存文件， True，每次返回的文件地址是一样的
+     * @return 录音文件的地址
+     */
+    public static File getAudioTmpFile(boolean isTmp) {
+        File dir = new File(getCacheDirFile(), "audio");
+        //noinspection ResultOfMethodCallIgnored
+        dir.mkdirs();
+        File[] files = dir.listFiles();
+        if (files != null && files.length > 0) {
+            for (File file : files) {
+                //noinspection ResultOfMethodCallIgnored
+                file.delete();
+            }
+        }
+
+        // aar
+        File path = new File(getCacheDirFile(), isTmp ? "tmp.mp3" : SystemClock.uptimeMillis() + ".mp3");
+        return path.getAbsoluteFile();
+    }
+
+    /**
+     * 显示一个Toast
+     *
+     * @param msg 字符串
+     */
+    public static void showToast(final String msg) {
+//        Toast.makeText(instance, msg, Toast.LENGTH_SHORT).show();
+        // Toast 只能在主线程中显示，所有需要进行线程转换，
+        // 保证一定是在主线程进行的show操作
+        Run.onUiAsync(new Action() {
+            @Override
+            public void call() {
+                Toast.makeText(instance, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     * 显示一个Toast
+     *
+     * @param msgId 传递的是字符串的资源
+     */
+    public static void showToast(@StringRes int msgId) {
+        showToast(instance.getString(msgId));
+    }
+
 }
