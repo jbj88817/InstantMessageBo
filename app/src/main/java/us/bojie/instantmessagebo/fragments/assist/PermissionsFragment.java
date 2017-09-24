f823dcc7efbbe782a58ed01db27f915e24e7a46f
Bojie Jiang <jiangbojie@gmail.com>
2017-09-24 12:13:53 -0700

    Applying permissions in PermissionsFragment

--- app/src/main/java/us/bojie/instantmessagebo/fragments/assist/PermissionsFragment.java
+++ app/src/main/java/us/bojie/instantmessagebo/fragments/assist/PermissionsFragment.java
@@ -1,7 +1,9 @@
 package us.bojie.instantmessagebo.fragments.assist;
 
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import pub.devrel.easypermissions.EasyPermissions;
 import us.bojie.instantmessagebo.R;
 import us.bojie.instantmessagebo.fragments.media.GalleryFragment;
 
@@ -30,13 +33,95 @@ public class PermissionsFragment extends BottomSheetDialogFragment {
                              @Nullable Bundle savedInstanceState) {
         // 获取布局中的控件
         View root = inflater.inflate(R.layout.fragment_permissions, container, false);
+        refreshState(root);
         return root;
     }
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // 复用即可
         return new GalleryFragment.TransStatusBottomSheetDialog(getContext());
+    }
+
+    /**
+     * 刷新我们的布局中的图片的状态
+     *
+     * @param root 跟布局
+     */
+    private void refreshState(View root) {
+        Context context = getContext();
+        root.findViewById(R.id.iv_state_permission_network)
+                .setVisibility(haveNetwork(context) ? View.VISIBLE : View.GONE);
+        root.findViewById(R.id.iv_state_permission_read)
+                .setVisibility(haveReadPerm(context) ? View.VISIBLE : View.GONE);
+        root.findViewById(R.id.iv_state_permission_write)
+                .setVisibility(haveWritePerm(context) ? View.VISIBLE : View.GONE);
+        root.findViewById(R.id.iv_state_permission_record_audio)
+                .setVisibility(haveRecordAudioPerm(context) ? View.VISIBLE : View.GONE);
+
+    }
+
+    /**
+     * 获取是否有网络权限
+     *
+     * @param context 上下文
+     * @return True则有
+     */
+    private static boolean haveNetwork(Context context) {
+        // 准备需要检查的网络权限
+        String[] perms = new String[]{
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.ACCESS_WIFI_STATE
+        };
+
+        return EasyPermissions.hasPermissions(context, perms);
+    }
+
+    /**
+     * 获取是否有外部存储读取权限
+     *
+     * @param context 上下文
+     * @return True则有
+     */
+    private static boolean haveReadPerm(Context context) {
+        // 准备需要检查的读取权限
+        String[] perms = new String[]{
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        };
+
+        return EasyPermissions.hasPermissions(context, perms);
+    }
+
+    /**
+     * 获取是否有外部存储写入权限
+     *
+     * @param context 上下文
+     * @return True则有
+     */
+    private static boolean haveWritePerm(Context context) {
+        // 准备需要检查的写入权限
+        String[] perms = new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+
+        return EasyPermissions.hasPermissions(context, perms);
+    }
+
+    /**
+     * 获取是否录音权限
+     *
+     * @param context 上下文
+     * @return True则有
+     */
+    private static boolean haveRecordAudioPerm(Context context) {
+        // 准备需要检查的录音权限
+        String[] perms = new String[]{
+                Manifest.permission.RECORD_AUDIO
+        };
+
+        return EasyPermissions.hasPermissions(context, perms);
     }
 
 }
