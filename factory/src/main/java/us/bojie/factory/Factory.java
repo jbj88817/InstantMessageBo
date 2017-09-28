b9b31b8ea276a0e398144eb36e398c4cfc9168eb
Bojie Jiang <jiangbojie@gmail.com>
2017-09-27 23:28:37 -0700

    OSS config and upload helper

---
+++ factory/src/main/java/us/bojie/factory/Factory.java
@@ -0,0 +1,14 @@
+package us.bojie.factory;
+
+import us.bojie.common.app.MyApplication;
+
+/**
+ * Created by bojiejiang on 9/25/17.
+ */
+
+public class Factory {
+    public static MyApplication app() {
+        return null;
+    }
+
+}
