fb3ca441829a57e79116152ec18629b889abf70c
Bojie Jiang <jiangbojie@gmail.com>
2017-10-23 23:08:01 -0700

    Background image and drawable setColorFilter

--- app/src/main/java/us/bojie/instantmessagebo/activities/AccountActivity.java
+++ app/src/main/java/us/bojie/instantmessagebo/activities/AccountActivity.java
@@ -2,7 +2,19 @@ package us.bojie.instantmessagebo.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
+
+import net.qiujuer.genius.ui.compat.UiCompat;
+
+import butterknife.BindView;
 import us.bojie.common.app.Activity;
 import us.bojie.common.app.Fragment;
 import us.bojie.instantmessagebo.R;
@@ -12,6 +24,8 @@ import us.bojie.instantmessagebo.fragments.account.RegisterFragment;
 
 public class AccountActivity extends Activity implements AccountTrigger {
 
+    @BindView(R.id.iv_bg)
+    ImageView mIvBg;
     private Fragment mCurFragment;
     private Fragment mLoginFragment;
     private Fragment mRegisterFragment;
@@ -34,11 +48,31 @@ public class AccountActivity extends Activity implements AccountTrigger {
     protected void initWidget() {
         super.initWidget();
 
+        // 初始化Fragment
         mCurFragment = mLoginFragment = new LoginFragment();
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.lay_container, mCurFragment)
                 .commit();
+
+        // 初始化背景
+        Glide.with(this)
+                .load(R.drawable.bg_src_tianjin)
+                .centerCrop()
+                .into(new ViewTarget<ImageView, GlideDrawable>(mIvBg) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource,
+                                                GlideAnimation<? super GlideDrawable> glideAnimation) {
+                        // 拿到glide的Drawable
+                        Drawable drawable = resource.getCurrent();
+                        // 使用适配类进行包装
+                        drawable = DrawableCompat.wrap(drawable);
+                        drawable.setColorFilter(UiCompat.getColor(getResources(), R.color.colorAccent),
+                                PorterDuff.Mode.SCREEN); // 设置着色的效果和颜色，蒙板模式
+                        // 设置给ImageView
+                        this.view.setImageDrawable(drawable);
+                    }
+                });
     }
 
     @Override
