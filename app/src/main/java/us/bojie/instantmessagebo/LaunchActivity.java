617b536541f3d79715483442e19317c4d279d080
Bojie Jiang <jiangbojie@gmail.com>
2017-10-29 11:38:44 -0700

    Set a ValueAnimator

--- app/src/main/java/us/bojie/instantmessagebo/LaunchActivity.java
+++ app/src/main/java/us/bojie/instantmessagebo/LaunchActivity.java
@@ -1,10 +1,24 @@
 package us.bojie.instantmessagebo;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.drawable.ColorDrawable;
+import android.util.Property;
+import android.view.View;
+
+import net.qiujuer.genius.res.Resource;
+import net.qiujuer.genius.ui.compat.UiCompat;
+
 import us.bojie.common.app.Activity;
 import us.bojie.instantmessagebo.activities.MainActivity;
 import us.bojie.instantmessagebo.fragments.assist.PermissionsFragment;
 
 public class LaunchActivity extends Activity {
+
+    private ColorDrawable mBgDrawable;
 
     @Override
     protected int getContentLayoutId() {
@@ -12,11 +26,75 @@ public class LaunchActivity extends Activity {
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void initWidget() {
+        super.initWidget();
+        // 拿到跟布局
+        View root = findViewById(R.id.activity_launch);
+        // 获取颜色
+        int color = UiCompat.getColor(getResources(), R.color.colorPrimary);
+        // 创建一个Drawable
+        ColorDrawable drawable = new ColorDrawable(color);
+        // 设置给背景
+        root.setBackground(drawable);
+        mBgDrawable = drawable;
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+        startAnim(0.8f, new Runnable() {
+            @Override
+            public void run() {
+                skip();
+            }
+        });
+    }
+
+    private void skip() {
         if (PermissionsFragment.haveAll(this, getSupportFragmentManager())) {
             MainActivity.show(this);
             finish();
         }
     }
+
+    /**
+     * 给背景设置一个动画
+     *
+     * @param endProgress 动画的结束进度
+     * @param endCallback 动画结束时触发
+     */
+    private void startAnim(float endProgress, final Runnable endCallback) {
+        // 获取一个最终的颜色
+        int finalColor = Resource.Color.WHITE; // UiCompat.getColor(getResources(), R.color.white);
+        // 运算当前进度的颜色
+        ArgbEvaluator evaluator = new ArgbEvaluator();
+        int endColor = (int) evaluator
+                .evaluate(endProgress, mBgDrawable.getColor(), finalColor);
+        // 构建一个属性动画
+        ValueAnimator valueAnimator = ObjectAnimator.ofObject(this, property, evaluator, endColor);
+        valueAnimator.setDuration(1500); //1.5s
+        valueAnimator.setIntValues(mBgDrawable.getColor(), endColor);
+        valueAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                // 结束时触发
+                endCallback.run();
+            }
+        });
+        valueAnimator.start();
+    }
+
+    private final Property<LaunchActivity, Object> property =
+            new Property<LaunchActivity, Object>(Object.class, "color") {
+                @Override
+                public Object get(LaunchActivity object) {
+                    return mBgDrawable.getColor();
+                }
+
+                @Override
+                public void set(LaunchActivity object, Object value) {
+                    object.mBgDrawable.setColor((Integer) value);
+                }
+            };
 }
