9d9ff24be08755676d381ff1e445797fe5487a96
Bojie Jiang <jiangbojie@gmail.com>
2017-11-11 23:26:30 -0800

    SearchView action

--- app/src/main/java/us/bojie/instantmessagebo/activities/SearchActivity.java
+++ app/src/main/java/us/bojie/instantmessagebo/activities/SearchActivity.java
@@ -1,8 +1,14 @@
 package us.bojie.instantmessagebo.activities;
 
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import us.bojie.common.app.ToolbarActivity;
 import us.bojie.instantmessagebo.R;
@@ -36,6 +42,52 @@ public class SearchActivity extends ToolbarActivity {
         type = bundle.getInt(EXTRA_TYPE);
         // 是搜索人或者搜索群
         return type == TYPE_USER || type == TYPE_GROUP;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // 初始化菜单
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search, menu);
+
+        // 找到搜索菜单
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        if (searchView != null) {
+            // 拿到一个搜索管理器
+            SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+            // 添加搜索监听
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    // 当点击了提交按钮的时候
+                    search(query);
+                    return true;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    // 当文字改变的时候，咱们不会及时搜索，只在为null的情况下进行搜索
+                    if (TextUtils.isEmpty(s)) {
+                        search("");
+                        return true;
+                    }
+                    return false;
+                }
+            });
+        }
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * 搜索的发起点
+     *
+     * @param query 搜索的文字
+     */
+    private void search(String query) {
 
     }
 }
