6a82201d94b83c536df20739f031db9e6469648b
Bojie Jiang <jiangbojie@gmail.com>
2017-11-12 16:07:40 -0800

    SearchUserFragment and layouts

--- app/src/main/java/us/bojie/instantmessagebo/fragments/search/SearchUserFragment.java
+++ app/src/main/java/us/bojie/instantmessagebo/fragments/search/SearchUserFragment.java
@@ -1,17 +1,39 @@
 package us.bojie.instantmessagebo.fragments.search;
 
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import butterknife.BindView;
 import us.bojie.common.app.PresenterFragment;
-import us.bojie.factory.presenter.BaseContract;
+import us.bojie.common.widget.EmptyView;
+import us.bojie.common.widget.PortraitView;
+import us.bojie.common.widget.recycler.RecyclerAdapter;
+import us.bojie.factory.model.card.UserCard;
+import us.bojie.factory.presenter.search.SearchContract;
+import us.bojie.factory.presenter.search.SearchUserPresenter;
 import us.bojie.instantmessagebo.R;
 import us.bojie.instantmessagebo.activities.SearchActivity;
 
 /**
  * 搜索人的界面实现
  */
-public class SearchUserFragment extends PresenterFragment
-        implements SearchActivity.SearchFragment {
+public class SearchUserFragment extends PresenterFragment<SearchContract.Presenter>
+        implements SearchActivity.SearchFragment, SearchContract.UserView {
 
+    @BindView(R.id.recycler)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.empty)
+    EmptyView mEmptyView;
+
+    private RecyclerAdapter<UserCard> mAdapter;
 
     public SearchUserFragment() {
         // Required empty public constructor
@@ -23,12 +45,69 @@ public class SearchUserFragment extends PresenterFragment
     }
 
     @Override
-    protected BaseContract.Presenter initPresenter() {
-        return null;
+    protected void initWidget(View root) {
+        super.initWidget(root);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.setAdapter(mAdapter = new RecyclerAdapter<UserCard>() {
+
+            @Override
+            protected int getItemViewType(int position, UserCard data) {
+                // 返回cell的布局id
+                return R.layout.cell_search_list;
+            }
+
+            @Override
+            protected ViewHolder<UserCard> onCreateViewHolder(View root, int viewType) {
+                return new SearchUserFragment.ViewHolder(root);
+            }
+        });
+
+        // 初始化占位布局
+        mEmptyView.bind(mRecyclerView);
+        setPlaceHolderView(mEmptyView);
     }
 
     @Override
     public void search(String content) {
+        // Activity->Fragment->Presenter->Net
+        mPresenter.search(content);
+    }
 
+    @Override
+    protected SearchContract.Presenter initPresenter() {
+        return new SearchUserPresenter(this);
+    }
+
+    @Override
+    public void onSearchDone(List<UserCard> userCards) {
+        mAdapter.replace(userCards);
+        // 如果有数据，则是OK，没有数据就显示空布局
+        mPlaceHolderView.triggerOkOrEmpty(mAdapter.getItemCount() > 0);
+    }
+
+    /**
+     * 每一个Cell的布局操作
+     */
+    class ViewHolder extends RecyclerAdapter.ViewHolder<UserCard> {
+        @BindView(R.id.iv_portrait)
+        PortraitView mPortraitView;
+        @BindView(R.id.tv_name)
+        TextView mName;
+        @BindView(R.id.iv_follow)
+        ImageView mFollow;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        protected void onBind(UserCard userCard) {
+            Glide.with(SearchUserFragment.this)
+                    .load(userCard.getPortrait())
+                    .centerCrop()
+                    .into(mPortraitView);
+            mName.setText(userCard.getName());
+            mFollow.setEnabled(userCard.isFollow());
+        }
     }
 }
