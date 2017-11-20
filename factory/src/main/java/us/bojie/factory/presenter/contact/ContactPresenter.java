5c0651feac10be03faacb85f634fa6e223efffb1
Bojie Jiang <jiangbojie@gmail.com>
2017-11-19 22:00:11 -0800

    Use refreshContacts

--- factory/src/main/java/us/bojie/factory/presenter/contact/ContactPresenter.java
+++ factory/src/main/java/us/bojie/factory/presenter/contact/ContactPresenter.java
@@ -2,11 +2,20 @@ package us.bojie.factory.presenter.contact;
 
 import android.support.annotation.NonNull;
 
+import com.raizlabs.android.dbflow.config.DatabaseDefinition;
+import com.raizlabs.android.dbflow.config.FlowManager;
 import com.raizlabs.android.dbflow.sql.language.SQLite;
+import com.raizlabs.android.dbflow.structure.database.DatabaseWrapper;
+import com.raizlabs.android.dbflow.structure.database.transaction.ITransaction;
 import com.raizlabs.android.dbflow.structure.database.transaction.QueryTransaction;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import us.bojie.factory.data.DataSource;
+import us.bojie.factory.data.helper.UserHelper;
+import us.bojie.factory.model.card.UserCard;
+import us.bojie.factory.model.db.AppDatabase;
 import us.bojie.factory.model.db.User;
 import us.bojie.factory.model.db.User_Table;
 import us.bojie.factory.persistence.Account;
@@ -42,5 +51,43 @@ public class ContactPresenter extends BasePresenter<ContactContract.View>
                         getView().onAdapterDataChanged();
                     }
                 }).execute();
+
+        // 加载网络数据
+        UserHelper.refreshContacts(new DataSource.Callback<List<UserCard>>() {
+            @Override
+            public void onDataNotAvailable(int strRes) {
+                // 网络失败，因为本地有数据，不管错误
+            }
+
+            @Override
+            public void onDataLoaded(List<UserCard> userCards) {
+                // 转换为User
+                final List<User> users = new ArrayList<>();
+                for (UserCard userCard : userCards) {
+                    users.add(userCard.build());
+                }
+
+                // 丢到事物中保存数据库
+                DatabaseDefinition definition = FlowManager.getDatabase(AppDatabase.class);
+                definition.beginTransactionAsync(new ITransaction() {
+                    @Override
+                    public void execute(DatabaseWrapper databaseWrapper) {
+                        FlowManager.getModelAdapter(User.class)
+                                .saveAll(users);
+                    }
+                }).build().execute();
+
+                // 网络的数据往往是新的，我们需要直接刷新到界面
+                getView().getRecyclerAdapter().replace(users);
+                getView().onAdapterDataChanged();
+            }
+        });
+
+        // TODO 问题：
+        // 1.关注后虽然存储数据库，但是没有刷新联系人
+        // 2.如果刷新数据库，或者从网络刷新，最终刷新的时候是全局刷新
+        // 3.本地刷新和网络刷新，在添加到界面的时候会有可能冲突；导致数据显示异常
+        // 4.如何识别已经在数据库中有这样的数据了
+
     }
 }
